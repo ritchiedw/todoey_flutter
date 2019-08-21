6f25672d1e94e0e55e48f1495151b897e4d66400
Dave Ritchie <ritchiedw@hotmail.com>
2019-08-21 21:51:18 +0100

    lifting state

--- lib/widget/tasks_tile.dart
+++ lib/widget/tasks_tile.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkboxCallback(bool checkboxState) {
+    setState(() {
+      isChecked = checkboxState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: new TaskCheckBox(isChecked, checkboxCallback),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckBox(this.checkBoxState, this.toggleCheckboxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState,
+      onChanged: toggleCheckboxState,
+      activeColor: Colors.lightBlueAccent,
     );
   }
 }
